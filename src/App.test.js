8a64f8ab2ea18f80ce3671b42ae4e0b719585b79
shekhar-solanki-techfrolic <shekhar@techfrolic.com>
2023-12-18 21:44:58 +0530

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
